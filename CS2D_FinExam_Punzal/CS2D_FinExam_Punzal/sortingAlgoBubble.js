f2ea45963726fe552e05e1a2bdb8b86daa8b89cc
punzalpatrickjoseph <punzalpatrickjoseph17@gmail.com>
2024-12-20 08:25:22 +0800

    sortingAlgoBubble.js

---
+++ CS2D_FinExam_Punzal/CS2D_FinExam_Punzal/sortingAlgoBubble.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
+
+// Example usage:
+let numbers = [60, 15, 11, 23, 13];
+console.log("Original Array:", numbers);
+console.log("Sorted Array:", bubbleSort(numbers));
